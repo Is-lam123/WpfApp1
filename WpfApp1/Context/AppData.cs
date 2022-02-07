2521bb053cc60ea9072f9814f3c18af8a6f598bb
Is-lam123 <isl.mirzaev111@gmail.com>
2022-02-07 14:32:00 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Context/AppData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.Context
+{
+   internal static class AppData
+    {
+        public static dbUserEntities2 db = new dbUserEntities2();
+    }
+}
